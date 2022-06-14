e03b6a425d77d2a05d3cb4a6749f257bb1d36570
chungmuwei <chungmuwei@gmail.com>
2022-06-14 22:53:42 +1000

    change optional argument, create unit class, scrape more info
    1. replace -m optional armument with -r
    because "CC" only means Camperdown/Darlington Campus, so it cannot represent all other on-campus location
    Now, "-r" specifies remote mode, and the default just search for any on-campus mode
    
    2. unit class
    since there are too many infomation about a unit, store all of them in a object will be more easy to manage
    
    3. print acadamic detail and enrolment rules

--- README.md
+++ README.md
@@ -3,18 +3,19 @@
 ## Introduction
 
 A python CLI tool to find information about the units of study provided by University of Sydney.
-This tool is under initial development. The current functionality is printing the unit overview.
+This tool is under initial development. The current functionality is printing the academic details, 
+enrolment requirements, and overview of the units.
 
 ## Manual
 
-usage: `findunit [-h] [-y YEAR] [-m {CC,RE}] unitcode [unitcode ...]`
+usage: `findunit [-h] [-y YEAR] [-r] unitcode [unitcode ...]`
 
-Print the overview of an unit of study at the University of Sydney
+Print the academic details, enrolment requirements, and overview of a list of unit of studies at the University of Sydney
 
 positional arguments:
 - unitcode: the unit code to find
 
 options:
--  -h, --help:                   show this help message and exit
--  -y YEAR, --year YEAR:         the year of the unit of study
--  -m {CC,RE}, --mode {CC,RE}:   the mode of the unit study
+-  -h, --help:              show this help message and exit
+-  -y YEAR, --year YEAR:    the year of the unit of study
+-  -r, --remote             remote/online delivery mode
---
+++ unit.py
@@ -0,0 +1,64 @@
+class unit:
+
+    __url: str
+    __name: str
+    __code: str
+    __credit_point: int
+
+    __prohibitions: str
+    __prerequisites: str
+    __corequisites: str
+    # __assumed_knowledge: str
+
+    __coordinator: str
+
+    __overview: str
+
+    def __init__(self, url, code, name, credit_point, overview) -> None:
+        self.__url = url
+        self.__code = code
+        self.__name = name
+        self.__credit_point = credit_point
+        self.__overview = overview
+    
+    def code(self) -> str:
+        return self.__code
+    
+    def name(self) -> str:
+        return self.__name
+    
+    def overview(self) -> str:
+        return self.__overview
+    
+    def credit_point(self) -> int:
+        return self.__credit_point
+    
+    def set_prohibitions(self, prohibitions: str) -> None:
+        self.__prohibitions = prohibitions
+    
+    def set_prerequisites(self, prerequisites: str) -> None:
+        self.__prerequisites = prerequisites
+    
+    def set_corequisites(self, corequisites: str) -> None:
+        self.__corequisites = corequisites
+    
+    # def set_assumed_knowledge(self, assumed_knowledge: str) -> None:
+    #     self.__assumed_knowledge = assumed_knowledge
+    
+    def __str__(self) -> str:
+        return f"""{self.code()}: {self.name()}
+Credit point:       |   {self.credit_point()}
+Prohibition:        |   {self.__prohibitions}
+Prerequisite:       |   {self.__prerequisites}
+Corequisite:        |   {self.__corequisites}
+
+Overview:
+{self.overview()}
+
+Source: {self.__url}
+
+"""
+    
+if __name__ == "__main__":
+    comp2022 = unit("comp2022")
+    print(comp2022.code())
